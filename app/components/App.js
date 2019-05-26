7f3dad94ed3e9a6009d2334c058306d037fa424b
Feng Jiang <fjiang91@gmail.com>
2019-05-26 01:22:13 -0400

    refactored app

---
+++ app/components/App.js
@@ -0,0 +1,35 @@
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import {campusSeed, studentSeed} from '../../seed.js';
+
+export default class App extends Component {
+
+  render() {
+    console.log(campusSeed, studentSeed);
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Junior Phase Final Project</Text>
+      </View>
+    );
+  }
+}
+
---
+++ seed.js
@@ -0,0 +1,77 @@
+export const campusSeed = [{
+  name: 'One Piece Elementary School',
+  imageUrl:'https://images.unsplash.com/photo-1542404937-2132aa1fa6fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+  address: 'Victor Plains, Wisokyburgh, 90566-7771',
+  description: 'Elementary School for One Piece Students'
+},
+{
+  name: 'One Piece Junior High',
+  imageUrl: 'https://images.unsplash.com/photo-1541691734047-8345ea5cf6ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+  address: 'Hoeger Mall, South Elvis, 53919-4257',
+  description: 'Junior High School for One Piece Students'
+},
+{
+  name: 'One Piece High School',
+  imageUrl: 'https://images.unsplash.com/photo-1547653872-052e3539decc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+  address: 'Skiles Walks, Roscoeview, 33263',
+  description: 'High School for One Piece Students'
+},
+{
+  name: 'One Piece University',
+  imageUrl: 'https://images.unsplash.com/photo-1527891751199-7225231a68dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+  address: 'Douglas Extension, McKenziehaven, 9590-4157',
+  description: 'Univerisity for One Piece Students'
+}
+];
+
+const emptyCampusSeed = {
+name: 'Empty Campus',
+imageUrl: 'https://images.unsplash.com/photo-1542906473-60340c54ec40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+address: 'Empty Campus Address',
+description: 'Empty Campus Description'
+};
+
+const emptyStudentSeed = {
+firstName: 'Random',
+lastName: 'Person',
+email: 'RandomPerson@OnePiece.com',
+gpa: 2
+}
+
+const duplicateCampusStudentSeed = {
+firstName: 'Sanji',
+lastName: 'Vinsmoke',
+email: 'SanjiVinsmoke@OnePiece.com',
+imageUrl: 'https://cdn.pixabay.com/photo/2018/10/08/01/05/male-3731782_1280.jpg',
+gpa: 3.1
+}
+
+export const studentSeed = [{
+firstName: 'Luffy',
+lastName: 'Monkey D.',
+email: 'MonkeyD.Luffy@OnePiece.com',
+imageUrl: 'https://cdn.pixabay.com/photo/2018/10/28/13/50/luffy-3778854_1280.jpg',
+gpa: 3.9
+},
+{
+firstName: 'Zoro',
+lastName: 'Roronoa',
+email: 'RoronoaZoro@OnePiece.com',
+imageUrl: 'https://cdn.pixabay.com/photo/2018/10/01/00/46/male-3715098_1280.jpg',
+gpa: 3.7
+},
+{
+firstName: 'Chopper',
+lastName: 'Tony Tony',
+email: 'TonyTonyChopper@OnePiece.com',
+imageUrl: 'https://cdn.pixabay.com/photo/2018/11/30/11/18/small-3847558_1280.jpg',
+gpa: 3.4
+},
+{
+firstName: 'Nami',
+lastName: 'Cat Burglar',
+email: 'Nami@OnePiece.com',
+imageUrl: 'https://cdn.pixabay.com/photo/2018/09/12/08/47/girl-3671577_1280.jpg',
+gpa: 3.8
+}
+];
